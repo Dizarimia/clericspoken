194ddc8fd1acf426d6994669abba1f690e76037b
Dizarimia <lillemor.sunnegard@outlook.com>
2023-01-05 23:19:15 +0000

    add templates

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://yrgaccce:YQMxbONQLk5LSJ4szzCEg64itSzBTqP2@balarama.db.elephantsql.com/yrgaccce"
+os.environ["SECRET_KEY"] = "8en6s*56pf+^6&ds1$1pdg0!e-4&npzrc$qoyx0^a0y)v6rga@"
+os.environ["CLOUDINARY_URL"] = "cloudinary://738314813239996:vUccGQmQsxSBfBTBc_hDDF0kDVc@dhdna3rfz"
